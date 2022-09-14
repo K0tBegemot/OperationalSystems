d132a06bde3d7b79969ec48efd695221cfcd2fe3
KotBegemot <bakons03@mail.ru>
2022-09-14 17:10:18 +0700

    Code for lab2

---
+++ lab2/lab2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define SUCCESS 0
+#define ERROR 1
+#define PTHREAD_CREATE_ERROR 0
+#define N 10
+
+void* printMessage() {
+    for (int i = 0; i < N; i++)
+    {
+        printf("New thread\n");
+    }
+}
+
+int main() {
+    pthread_t new_thread;
+    int create_result = pthread_create(&new_thread, NULL, printMessage, NULL);
+
+    if (create_result != PTHREAD_CREATE_ERROR) {
+        printf("pthread_create error: couldn't create thread\n");
+        return ERROR;
+    }
+
+    pthread_join(new_thread, NULL);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Main thread\n");
+    }
+
+    pthread_exit(NULL);
+}
+
