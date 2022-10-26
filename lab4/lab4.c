5ff53d9c122dcde2bbf22c80afd120a711f25e97
KotBegemot <bakons03@mail.ru>
2022-10-26 18:10:51 +0700

    Modified verion of lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -6,6 +6,8 @@
 
 #define CHILD_SLEEP_TIME 5
 #define MAIN_SLEEP_TIME 2
+#define PTHREAD_CREATE_SUCCESS_VALUE 0
+#define PTHREAD_CANCEL_SUCCESS_VALUE 0
 
 void *threadFunction(void *string)
 {
@@ -38,34 +40,33 @@ int main()
     void *threadRes;
     char *string = "Child thread watching over you. Ha ha ha...\n";
     int ret = pthread_create(&thread, NULL, threadFunction, string);
-    if (ret != 0)
+    if (ret != PTHREAD_CREATE_SUCCESS_VALUE)
     {
         printError("Error. Child thread wasn't created\n");
+        return 0;
     }
     sleep(MAIN_SLEEP_TIME);
-    ret = pthread_cancel(thread);
-    if (ret != 0)
+    int ret3 = pthread_cancel(thread);
+    if (ret3 != PTHREAD_CANCEL_SUCCESS_VALUE)
     {
         printError("Error occured in function pthread_cancel. There is no such thread\n");
+        return 0;
     }
     int ret1 = pthread_join(thread, &threadRes);
     if (threadRes == PTHREAD_CANCELED)
     {
         printMessage("Thread was cancelled. Ok\n");
+        return 0;
     }
-    else
+    if (ret1 == 0 && ret3 == 0)
     {
-        if (ret1 == 0 && ret == 0)
-        {
-            printMessage("Child thread returned earlier then pthread_cancel was executed\n");
-        }
-        else
-        {
-            if (ret1 == ESRCH && ret == ESRCH)
-            {
-                printError("Errors in function pthread_cancel and pthread_join. There is no such thread\n");
-            }
-        }
+        printMessage("Child thread returned earlier then pthread_cancel was executed\n");
+        return 0;
+    }
+    if (ret1 == ESRCH || ret3 == ESRCH)
+    {
+        printError("Errors in function pthread_cancel and pthread_join. There is no such thread\n");
+        return 0;
     }
     return 0;
 }
